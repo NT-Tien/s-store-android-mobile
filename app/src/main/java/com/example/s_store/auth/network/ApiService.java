59ac116845e61b0879168384a13d76ce5312d3a2
vphoa34 <vphoa34@gmail.com>
2024-06-13 03:25:15 +0700

    feature: sign up

--- app/src/main/java/com/example/s_store/auth/network/ApiService.java
+++ app/src/main/java/com/example/s_store/auth/network/ApiService.java
@@ -2,6 +2,8 @@ package com.example.s_store.auth.network;
 
 import com.example.s_store.auth.models.LoginRequest;
 import com.example.s_store.auth.models.LoginResponse;
+import com.example.s_store.auth.models.SignUpRequest;
+import com.example.s_store.auth.models.SignUpResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -10,4 +12,7 @@ import retrofit2.http.POST;
 public interface ApiService {
     @POST("/auth/login")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @POST("/auth/register")
+    Call<SignUpResponse> signUp(@Body SignUpRequest signUpRequest);
 }
